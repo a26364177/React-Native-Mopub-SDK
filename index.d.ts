63eda784faa8cd9bcfc21ece0c0bc0625859c731
Hyeonsu Lee <incleaf@gmail.com>
2021-11-16 22:18:37 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,8 @@ import { IAdLibSDK } from './src/AdLibSDK';
 import { IRNMoPubInterstitial } from "./src/RNMoPubInterstitial";
 import { IRNAppOpenAd } from "./src/RNAppOpenAd";
 import { IRNMoPubRewardedVideo } from "./src/RNMoPubRewardedVideo";
-import { IRNNativeAdViewProps, type NativeAdDataType } from './src/RNNativeAdView'
+import { IRNNativeAdViewProps } from './src/RNNativeAdView'
+import type { NativeAdDataType } from './src/RNNativeAdView'
 
 declare module "react-native-mopub-sdk" {
   export const AdLibSDK: IAdLibSDK;
